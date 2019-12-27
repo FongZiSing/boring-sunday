097e84fc1c00b21134f02aeb1e26cf5bd44c8ddb
DreamWallower <1052054788@qq.com>
2019-12-27 20:59:46 +0800

    添加点击事件

--- src/digitalRain.js
+++ src/digitalRain.js
@@ -2,12 +2,13 @@ export default class digitalRain {
     constructor(canvasId, fontSize, fontFamily, backgroundColor, digitalColor) {
         this.canvasId = canvasId;
         this.cvs = document.getElementById(canvasId);
-        this.width = window.screen.width;
-        this.height = window.screen.height;
+        this.width = Math.max(window.innerWidth, window.screen.width);
+        this.height = Math.max(window.innerHeight, window.screen.height);
 
         this.fontSize = fontSize;
         this.fontFamily = fontFamily;
         this.backgroundColor = backgroundColor;
+        this.backgroundColorString = 'rgba(' + backgroundColor.join(', ') + ', 0.15)';
         this.digitalColor = digitalColor;
 
         this.columns = Math.ceil(this.width / fontSize);
@@ -28,23 +29,51 @@ export default class digitalRain {
     }
 
     __onload() {
-        let timer = setInterval(this.render.bind(this), 50);
+        this.timer = setInterval(this.render.bind(this), 50);
     }
 
-    render() {
-        this.ctx.fillStyle = this.backgroundColor;
+    unload() {
+        this.timer && this.__unload();
+    }
+
+    __unload() {
+        clearInterval(this.timer);
+        let endingColor = 'rgba(' + this.backgroundColor.join(', ') + ', ';
+        for (let i = 1; i <= 20; ++i) {
+            setTimeout(() => {
+                this.backgroundRender(endingColor + (0.15 + (0.85 * i / 20)) + ')');
+                this.contentRender();
+            }, (50 + i) * i);
+        }
+        for (let i = 0; i <= 20; ++i) {
+            setTimeout(() => {
+                this.backgroundRender(endingColor + (0.15 + (0.85 * i / 20)) + ')');
+            }, (50 + i) * i);
+        }
+        this.timer = 0;
+
+    }
+
+    backgroundRender(color = this.backgroundColorString) {
+        this.ctx.fillStyle = color;
         this.ctx.fillRect(0, 0, this.width, this.height);
-        this.ctx.fillStyle = "#820014";
+    }
+
+    contentRender() {
+        this.ctx.fillStyle = this.digitalColor;
         this.ctx.font = this.fontSize + "px " + this.fontFamily;
         for (let i = 0; i < this.columns; i++) {
             let figure = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'][Math.floor(Math.random() * 12)];
-
-            /*绘制数字(核心部分)*/
             this.ctx.fillText(figure, i * this.fontSize, this.drop[i] * this.fontSize);
             if (this.drop[i] * this.fontSize > this.height || Math.random() > 0.95) {
                 this.drop[i] = 0;
             }
             this.drop[i]++;
         }
+    }
+
+    render() {
+        this.backgroundRender();
+        this.contentRender();
     }
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import digitalRain from './digitalRain.js';
 
-const rain = new digitalRain('background', 16, "Agency FB", "rgba(255, 255, 255, 0.15)", "#820014");
+const rain = new digitalRain('background', 16, "Agency FB", [255, 255, 255], "#820014");
 rain.onload();
+
+let btn = document.getElementById("button-start");
+btn.addEventListener('click', () => rain.unload());
