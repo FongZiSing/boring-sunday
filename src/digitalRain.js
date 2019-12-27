bfd23e978bf1ec646a9d345687898506d6e9b21b
DreamWallower <1052054788@qq.com>
2019-12-27 16:25:05 +0800

    test

--- src/digitalRain.js
+++ src/digitalRain.js
@@ -1,4 +1,4 @@
-export default class digitalRain{
+export default class digitalRain {
     constructor(canvasId, fontSize, fontFamily, backgroundColor, digitalColor) {
         this.canvasId = canvasId;
         this.cvs = document.getElementById(canvasId);
@@ -9,22 +9,22 @@ export default class digitalRain{
         this.fontFamily = fontFamily;
         this.backgroundColor = backgroundColor;
         this.digitalColor = digitalColor;
-    
+
         this.columns = Math.ceil(this.width / fontSize);
         this.drop = [];
 
-        for(let i = 0; i < this.columns; ++i) {
+        for (let i = 0; i < this.columns; ++i) {
             this.drop[i] = Math.floor(Math.random() * 18);
         }
     }
 
     onload() {
-        if(this.cvs.getContext) {
+        if (this.cvs.getContext) {
             this.cvs.width = this.width;
             this.cvs.height = this.height;
             this.ctx = this.cvs.getContext('2d');
             window.onload = this.__onload.bind(this);
-        }        
+        }
     }
 
     __onload() {
@@ -38,7 +38,7 @@ export default class digitalRain{
         this.ctx.font = this.fontSize + "px " + this.fontFamily;
         for (let i = 0; i < this.columns; i++) {
             let figure = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', '♔', '♕', '♠', '♣', '♥', '♦'][Math.floor(Math.random() * 18)];
-            
+
             /*绘制数字(核心部分)*/
             this.ctx.fillText(figure, i * this.fontSize, this.drop[i] * this.fontSize);
             if (this.drop[i] * this.fontSize > this.height || Math.random() > 0.95) {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { digitalRain } from './digitalRain.js';
+import digitalRain from './digitalRain';
 
 const rain = new digitalRain('digitalRain', 16, "Agency FB", "rgba(255, 255, 255, 0.15)", "#820014");
 rain.onload();
